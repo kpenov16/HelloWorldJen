b106aa3f3e0ca5ef3d3c26d59c076a70af885603
Kaloyan Penov <s133967@student.dtu.dk>
2019-01-07 22:30:40 +0100

    - first commit

---
+++ src/test/java/HelloWorldJenTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldJenTest {
+
+
+    @Test
+    public void nothing(){
+        assertEquals("2","2");
+    }
+}
